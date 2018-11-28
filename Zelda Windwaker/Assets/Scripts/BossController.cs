59e7fd0cc83e1668da828b2ef4a00c116d2b3cbb
Tiesto Schouten <22230@ma-web.nl>
2018-11-28 11:30:54 +0100

    Boss basis

--- Zelda Windwaker/Assets/Scripts/BossController.cs
+++ Zelda Windwaker/Assets/Scripts/BossController.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+public class BossController : MonoBehaviour
+{
 
-public class BossController : MonoBehaviour {
+    public Transform Player;
+    int MoveSpeed = 4;
+    int MaxDist = 15;
+    int MinDist = 10;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.LookAt(Player);
+
+        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        {
+
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+
+
+            if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+            {
+                //Space for optional function
+            }
+
+        }
+    }
 }
