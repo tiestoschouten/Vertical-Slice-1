c6be25377d50f81acdc6ff1ce35be2b365a091d5
Alexbroking <alexbroking@gmail.com>
2018-11-28 13:58:16 +0100

    Player Movement

---
+++ Zelda Windwaker/Assets/Scripts/Utility/PointTowards.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CameraTest: MonoBehaviour
+{
+
+    [SerializeField]
+    private Transform Player;
+
+    private int MoveSpeed = 0;
+    private int MaxDist = 15;
+    private int MinDist = 15;
+
+
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.LookAt(Player);
+
+        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        {
+
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+
+
+            if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+            {
+                //Space for optional function
+            }
+
+        }
+    }
+}
+
